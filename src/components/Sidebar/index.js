74da44be8390996cd9ddda34f287fc01fadbc729
ignacio <chonathomp@gmail.com>
2023-03-23 11:49:56 -0300

    phone comp

--- src/components/Sidebar/index.js
+++ src/components/Sidebar/index.js
@@ -2,15 +2,19 @@ import {Link, NavLink} from 'react-router-dom'
 import './index.scss';
 import LogoS from '../../assets/images/IILOGO.png'
 import {FontAwesomeIcon} from '@fortawesome/react-fontawesome'
-import {faEnvelope, faHome, faSuitcase, faUser,} from '@fortawesome/free-solid-svg-icons'
+import {faBars, faClose, faEnvelope, faHome, faSuitcase, faUser,} from '@fortawesome/free-solid-svg-icons'
 import {faGithub, faLinkedin} from '@fortawesome/free-brands-svg-icons'
+import { useState } from 'react';
 
-const Sidebar = () => (
+const Sidebar = () => {
+
+    const [showNav, setShowNav] = useState(false);
+    return(
     <div className='nav-bar'>
         <Link className = 'logo' to ='/'>
            <img src={LogoS} alt='logo' />
         </Link>
-        <nav>
+        <nav className={showNav ? 'mobile-show': ''}>
             <NavLink exact="true" activeclassname='active' to='/'>
                 <FontAwesomeIcon icon={faHome} color='#4d4d4e' />
             </NavLink>
@@ -23,6 +27,13 @@ const Sidebar = () => (
             <NavLink exact="true" activeclassname='active' className="contact-link" to='/contact'>
                 <FontAwesomeIcon icon={faEnvelope} color='#4d4d4e' />
             </NavLink>
+            <FontAwesomeIcon
+                onClick={() => setShowNav(false)}
+                icon={faClose}
+                color="#87ceed"
+                size='3x'
+                className='close-icon'
+            />
         </nav>
         <ul>
             <li>
@@ -44,6 +55,14 @@ const Sidebar = () => (
                 </a>
             </li>
         </ul>
+        <FontAwesomeIcon 
+        onClick={() => setShowNav(true)}
+        icon={faBars}
+        color='#87ceed'
+        size='3x'
+        className='hamburger-icon'
+        />
     </div>
     )
+}
 export default Sidebar
